ad28b615ff40933ddf5ee267e373d7616d45c5b8
Cayman <caymannava@gmail.com>
2026-01-12 10:57:25 -0500

    fix: processHistoricalRootsUpdate

--- src/state_transition/epoch/process_historical_roots_update.zig
+++ src/state_transition/epoch/process_historical_roots_update.zig
@@ -6,25 +6,19 @@ const preset = @import("preset").preset;
 const Root = types.primitive.Root.Type;
 
 pub fn processHistoricalRootsUpdate(cached_state: *CachedBeaconState, cache: *const EpochTransitionCache) !void {
-    const state = &cached_state.state;
+    const state = cached_state.state;
     const next_epoch = cache.current_epoch + 1;
 
     // set historical root accumulator
     if (next_epoch % @divFloor(preset.SLOTS_PER_HISTORICAL_ROOT, preset.SLOTS_PER_EPOCH) == 0) {
-        var block_roots_view = try state.blockRoots();
-        var block_roots: Root = undefined;
-        try block_roots_view.hashTreeRoot(&block_roots);
-
-        var state_roots_view = try state.stateRoots();
-        var state_roots: Root = undefined;
-        try state_roots_view.hashTreeRoot(&state_roots);
-
+        const block_roots = try state.blockRootsRoot();
+        const state_roots = try state.stateRootsRoot();
         var root: Root = undefined;
         // HistoricalBatchRoots = Non-spec'ed helper type to allow efficient hashing in epoch transition.
         // This type is like a 'Header' of HistoricalBatch where its fields are hashed.
         try types.phase0.HistoricalBatchRoots.hashTreeRoot(&.{
-            .block_roots = block_roots,
-            .state_roots = state_roots,
+            .block_roots = block_roots.*,
+            .state_roots = state_roots.*,
         }, &root);
         var historical_roots = try state.historicalRoots();
         try historical_roots.pushValue(&root);
