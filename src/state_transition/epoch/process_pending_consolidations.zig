936ec937bd779ae1ed45e3e8d6e253e5a097d598
Cayman <caymannava@gmail.com>
2026-01-12 09:33:08 -0500

    fix: processPendingConsolidations

--- src/state_transition/epoch/process_pending_consolidations.zig
+++ src/state_transition/epoch/process_pending_consolidations.zig
@@ -1,3 +1,4 @@
+const Allocator = @import("std").mem.Allocator;
 const types = @import("consensus_types");
 const CachedBeaconState = @import("../cache/state_cache.zig").CachedBeaconState;
 const EpochTransitionCache = @import("../cache/epoch_transition_cache.zig").EpochTransitionCache;
@@ -5,46 +6,48 @@ const decreaseBalance = @import("../utils/balance.zig").decreaseBalance;
 const increaseBalance = @import("../utils/balance.zig").increaseBalance;
 
 /// also modify balances inside EpochTransitionCache
-pub fn processPendingConsolidations(cached_state: *CachedBeaconState, cache: *EpochTransitionCache) !void {
+pub fn processPendingConsolidations(_: Allocator, cached_state: *CachedBeaconState, cache: *EpochTransitionCache) !void {
     const epoch_cache = cached_state.getEpochCache();
-    var state = &cached_state.state;
+    var state = cached_state.state;
     const next_epoch = epoch_cache.epoch + 1;
-
+    var next_pending_consolidation: usize = 0;
     var validators = try state.validators();
     var balances = try state.balances();
+
     var pending_consolidations = try state.pendingConsolidations();
+    var pending_consolidations_it = pending_consolidations.iteratorReadonly(0);
     const pending_consolidations_length = try pending_consolidations.length();
+    for (0..pending_consolidations_length) |_| {
+        const pending_consolidation = try pending_consolidations_it.nextValue(undefined);
+        const source_index = pending_consolidation.source_index;
+        const target_index = pending_consolidation.target_index;
+        const source_validator = try validators.get(source_index);
 
-    var processed: usize = 0;
-    while (processed < pending_consolidations_length) : (processed += 1) {
-        const pending_consolidation = try pending_consolidations.get(processed);
-        const source_index = try pending_consolidation.get("source_index");
-        const target_index = try pending_consolidation.get("target_index");
-
-        var source_validator = try validators.get(@intCast(source_index));
-        const slashed = try source_validator.get("slashed");
-        if (slashed) {
+        if (try source_validator.get("slashed")) {
+            next_pending_consolidation += 1;
             continue;
         }
 
-        const withdrawable_epoch = try source_validator.get("withdrawable_epoch");
-        if (withdrawable_epoch > next_epoch) {
+        if ((try source_validator.get("withdrawable_epoch")) > next_epoch) {
             break;
         }
 
-        const source_balance = try balances.get(@intCast(source_index));
-        const source_effective_balance = @min(source_balance, try source_validator.get("effective_balance"));
+        // Calculate the consolidated balance
+        const source_effective_balance = @min(try balances.get(source_index), try source_validator.get("effective_balance"));
 
-        try decreaseBalance(state, source_index, source_effective_balance);
-        try increaseBalance(state, target_index, source_effective_balance);
+        // Move active balance to target. Excess balance is withdrawable.
+        try decreaseBalance(&state, source_index, source_effective_balance);
+        try increaseBalance(&state, target_index, source_effective_balance);
         if (cache.balances) |cached_balances| {
-            cached_balances.items[@intCast(source_index)] -= source_effective_balance;
-            cached_balances.items[@intCast(target_index)] += source_effective_balance;
+            cached_balances.items[source_index] -= source_effective_balance;
+            cached_balances.items[target_index] += source_effective_balance;
         }
+
+        next_pending_consolidation += 1;
     }
 
-    if (processed > 0) {
-        const truncated = try pending_consolidations.sliceFrom(processed);
-        try state.setPendingConsolidations(truncated);
+    if (next_pending_consolidation > 0) {
+        const new_pending_consolidations = try pending_consolidations.sliceFrom(next_pending_consolidation);
+        try state.setPendingConsolidations(new_pending_consolidations);
     }
 }
