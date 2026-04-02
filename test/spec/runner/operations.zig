1f717e8547840c90e10d64c9959cc50c096fc037
Rahul Guha <19rahul2003@gmail.com>
2026-04-02 17:49:38 +0530

    zig fmt and test:config

--- test/spec/runner/operations.zig
+++ test/spec/runner/operations.zig
@@ -106,7 +106,6 @@ pub fn TestCase(comptime fork: ForkSeq, comptime operation: Operation) type {
         const Self = @This();
 
         pub fn execute(allocator: std.mem.Allocator, dir: std.fs.Dir) !void {
-
             const pool_size = if (active_preset == .mainnet) 10_000_000 else 1_000_000;
             var pool = try Node.Pool.init(allocator, pool_size);
             defer pool.deinit();
